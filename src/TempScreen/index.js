6bc396d3628659b062344954a2c1b5511d13248b
ddhearly <dheadearly@student.telkomuniversity.ac.id>
2021-01-07 14:31:13 +0700

    nambah Heartscreen dan Tempscreen

---
+++ src/TempScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TempScreen = () => {
+    return(
+        <View>
+            <Text>Apaaa</Text>
+        </View>
+    )
+}
+
+export default TempScreen;
