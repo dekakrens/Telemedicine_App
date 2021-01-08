b29b7b1bf08d75bb7b92417c225d9d84e5f6f3d0
ddhearly <dheadearly@student.telkomuniversity.ac.id>
2021-01-08 13:10:24 +0700

    Berhasil write data pada BLE Device

--- src/WeightScreen/index.js
+++ src/WeightScreen/index.js
@@ -1,12 +1,119 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity 
+} from 'react-native';
 
-const WeightScreen = () => {
-    return(
-        <View>
-            <Text>Ini Weight Screen</Text>
-        </View>
-    )
-}
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import BleManager from "react-native-ble-manager"
+import { stringToBytes } from 'convert-string';
 
-export default WeightScreen;
+
+export default class WeightScreen extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.writeBtn1 = this.writeBtn1.bind(this);
+    this.writeBtn2 = this.writeBtn2.bind(this);
+  }
+
+  async writeBtn1(){
+    const id = '10:52:1C:68:14:E2'
+    const characteristicID = '00004f25-0000-1000-8000-00805f9b34fb'
+    const serviceID = '181D'
+    
+    
+
+    const data = stringToBytes('1');
+    console.log(data)
+
+    BleManager.retrieveServices(id).then((peripheralInfo) => {
+      //console.log(peripheralInfo);
+
+      setTimeout(() => {
+        BleManager.startNotification(id, serviceID, characteristicID).then(() => {
+          console.log('Started notification on ' + id);
+          setTimeout(() => {
+            BleManager.write(id, serviceID, characteristicID, data).then(() => {
+              console.log("Success Write");
+              
+            }).catch((e) => {
+              console.log(e)
+            });
+
+          }, 500);
+        }).catch((error) => {
+          console.log('Notification error', error);
+        });
+      }, 200);
+    });
+
+  }
+  async writeBtn2(){
+    const id = '10:52:1C:68:14:E2'
+    const characteristicID = '00004f25-0000-1000-8000-00805f9b34fb'
+    const serviceID = '181D'
+    
+    
+
+    const data = stringToBytes('2');
+    console.log(data)
+
+    BleManager.retrieveServices(id).then((peripheralInfo) => {
+      //console.log(peripheralInfo);
+
+      setTimeout(() => {
+        BleManager.startNotification(id, serviceID, characteristicID).then(() => {
+          console.log('Started notification on ' + id);
+          setTimeout(() => {
+            BleManager.write(id, serviceID, characteristicID, data).then(() => {
+              console.log("Success Write");
+              
+            }).catch((e) => {
+              console.log(e)
+            });
+
+          }, 500);
+        }).catch((error) => {
+          console.log('Notification error', error);
+        });
+      }, 200);
+    });
+
+  }
+
+  render(){
+    return (
+      <View>
+        <TouchableOpacity style={styles.connectBtn} onPress={this.writeBtn1}>
+          <Text style={{fontSize: 40}}>Write 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.connectBtn} onPress={this.writeBtn2}>
+          <Text style={{fontSize: 40}}>Write 2</Text>
+        </TouchableOpacity>
+      </View>
+      );
+  }
+  
+};
+
+const styles = StyleSheet.create({
+  connectBtn: {
+    alignSelf: "center",
+    borderColor: "red",
+    borderWidth: 1,
+    marginTop: "20%",
+    
+  }
+});
---
+++ src/WiFiBTScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WiFiBTScreen= () => {
+    return(
+        <View>
+            <Text>Ini WiFiBTScreen</Text>
+        </View>
+    )
+}
+
+export default WiFiBTScreen;
