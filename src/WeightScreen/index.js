7d1d901af6511c16293fc2ae35e32503e17d59e7
Deny Kurniawan <dekakrens@gmail.com>
2021-01-08 15:16:21 +0700

    fixing notification receive

--- src/WeightScreen/index.js
+++ src/WeightScreen/index.js
@@ -9,15 +9,8 @@ import {
   TouchableOpacity 
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import BleManager from "react-native-ble-manager"
-import { stringToBytes } from 'convert-string';
+import { stringToBytes, bytesToString } from 'convert-string';
 import { NativeModules, NativeEventEmitter } from "react-native";
  
 const BleManagerModule = NativeModules.BleManager;
@@ -32,58 +25,68 @@ export default class WeightScreen extends React.Component {
     this.writeBtn2 = this.writeBtn2.bind(this);
     this.notifBtn = this.notifBtn.bind(this);
   }
+
   async notifBtn() {
-    const id = '10:52:1C:68:14:E2'
-    const characteristicID = '00004f25-0000-1000-8000-00805f9b34fb'
-    const serviceID = '181D'
-  await BleManager.startNotification(id, serviceID, characteristicID);
+    const id = "24:6F:28:15:87:DA"
+    const characteristicID = '1006'
+    const serviceID = '180D'
+  await BleManager.startNotification(id, serviceID, '1141');
   // Add event listener
   bleManagerEmitter.addListener(
     "BleManagerDidUpdateValueForCharacteristic",
-    ({ value, id, serviceID, characteristicID }) => {
+    ({ value}) => {
       // Convert bytes array to string
-      const data = value;
+      const data = bytesToString(value);
       console.log('Received ' +data);
     }
   );
   // Actions triggereng BleManagerDidUpdateValueForCharacteristic event
 }
   async writeBtn1(){
-    const id = '10:52:1C:68:14:E2'
-    const characteristicID = '00004f25-0000-1000-8000-00805f9b34fb'
-    const serviceID = '181D'
+    const id = "24:6F:28:15:87:DA"
+    const characteristicID = '78604f25-789e-432e-b949-6fb2306fd5d7'
+    const serviceID = '180D'
     
-    
-
     const data = stringToBytes('1');
     console.log(data)
 
     BleManager.retrieveServices(id).then((peripheralInfo) => {
       //console.log(peripheralInfo);
-
       setTimeout(() => {
-        BleManager.startNotification(id, serviceID, characteristicID).then(() => {
-          console.log('Started notification on ' + id);
-          setTimeout(() => {
-            BleManager.write(id, serviceID, characteristicID, data).then(() => {
-              console.log("Success Write");
-              
-            }).catch((e) => {
-              console.log(e)
-            });
-
-          }, 500);
-        }).catch((error) => {
-          console.log('Notification error', error);
+        BleManager.write(id, serviceID, characteristicID, data).then(() => {
+          console.log("Success Write");
+          
+        }).catch((e) => {
+          console.log(e)
         });
-      }, 200);
+
+      }, 500);
+
+      BleManager.startNotification(id, serviceID, '1006');
+  
+  //     bleManagerEmitter.addListener(
+  //       "BleManagerDidUpdateValueForCharacteristic",
+  //       ( value, id, serviceID, '1006' ) => {
+  //         const data = value;
+  //         console.log('Received ' +data);
+  //       }
+  // );
+
+      // setTimeout(() => {
+      //   BleManager.startNotification(id, serviceID, '1006').then(() => {
+      //     console.log('Started notification on ' + id);
+          
+      //   }).catch((error) => {
+      //     console.log('Notification error', error);
+      //   });
+      // }, 200);
     });
 
   }
   async writeBtn2(){
-    const id = '10:52:1C:68:14:E2'
-    const characteristicID = '00004f25-0000-1000-8000-00805f9b34fb'
-    const serviceID = '181D'
+    const id = "24:6F:28:15:87:DA"
+    const characteristicID = '78604f25-789e-432e-b949-6fb2306fd5d7'
+    const serviceID = '180D'
     
     
 
